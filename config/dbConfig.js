efd54ca23a1907fd8d88c97b9e6373f1fcd8e209
Un-Known-glitch <gayanughimire69@gmail.com>
2025-07-09 09:14:25 +0545

    deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -14,18 +14,18 @@ module.exports = {
  },
 
 };
-module.exports = {
- HOST: "localhost",
- USER: "root",
- PASSWORD:"",
- DB: "blogs_db",
- dialect: "mysql",
- pool:{
-    max :5,
-    min:0,
-    acquire:30000,
-    idle:10000,
+// module.exports = {
+//  HOST: "localhost",
+//  USER: "root",
+//  PASSWORD:"",
+//  DB: "blogs_db",
+//  dialect: "mysql",
+//  pool:{
+//     max :5,
+//     min:0,
+//     acquire:30000,
+//     idle:10000,
 
- },
+//  },
 
-};
+// };
