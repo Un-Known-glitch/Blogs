c928f019ee02a956a81e9b6e1a7fe03547f7a476
Un-Known-glitch <gayanughimire69@gmail.com>
2025-07-09 09:11:30 +0545

    deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,19 @@
-// module.exports = {
-//  HOST: "${{RAILWAY_PRIVATE_DOMAIN}}",
-//  USER: "root",
-//  PASSWORD:"ZxAGqxDWGfmHcXbmGApTijZZhlFMXaFN",
-//  DB: "railway",
-//  dialect: "mysql",
-//  pool:{
-//     max :5,
-//     min:0,
-//     acquire:30000,
-//     idle:10000,
+module.exports = {
+ HOST: "${{RAILWAY_PRIVATE_DOMAIN}}",
+ USER: "root",
+ PASSWORD:"ZxAGqxDWGfmHcXbmGApTijZZhlFMXaFN",
+ DB: "railway",
+ dialect: "mysql",
+  port: 3306,
+ pool:{
+    max :5,
+    min:0,
+    acquire:30000,
+    idle:10000,
 
-//  },
+ },
 
-// };
+};
 module.exports = {
  HOST: "localhost",
  USER: "root",
