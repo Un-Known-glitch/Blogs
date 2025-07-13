61ae765969f404fea0dc908b3ab7729ef67713d4
Un-Known-glitch <gayanughimire69@gmail.com>
2025-07-13 19:45:37 +0545

    commit

--- model/index.js
+++ model/index.js
@@ -1,42 +1,38 @@
 const dbConfig = require("../config/dbConfig");
-const {Sequelize,DataTypes}= require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD,{
-host : dbConfig.HOST,
- dialect : dbConfig.dialect,
-//  port:3306,
- pool:{
+// Initialize Sequelize using full connection URL from config.url
+const sequelize = new Sequelize(dbConfig.url, {
+  dialect: dbConfig.dialect,
+  pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle:dbConfig.pool.idle,
- },
+    idle: dbConfig.pool.idle,
+  },
 });
 
 sequelize
-.authenticate()
-.then(()=>{
-    console.log("Connected !!");
-})
-.catch((err)=>{
-    console.log("Error" + err);
-})
+  .authenticate()
+  .then(() => {
+    console.log("Connected to the database successfully!");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const db = {};
-db.Sequelize=Sequelize;
-db.sequelize=sequelize;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 db.blogs = require("./blogModel.js")(sequelize, DataTypes);
 db.users = require("./usersModel.js")(sequelize, DataTypes);
 
-const users = require("./usersModel")(sequelize, DataTypes);
-const blogs = require("./blogModel")(sequelize,DataTypes);
+db.blogs.belongsTo(db.users, { foreignKey: "userId" });
+db.users.hasMany(db.blogs, { foreignKey: "userId" });
 
-db.blogs.belongsTo(db.users, { foreignKey: "userId" } );
-db.users.hasMany(db.blogs, { foreignKey: "userId" } );
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("Database synchronized");
+});
 
-db.sequelize.sync({force: false}).then(()=>{
-    console.log("yes re-synce done");
- 
-}); 
 module.exports = db;
